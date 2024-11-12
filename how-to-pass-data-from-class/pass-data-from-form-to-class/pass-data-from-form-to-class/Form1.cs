b1ef1abc7b9a6423e117546ba1dfd63dd7fbedc9
cerongithub <developerceron@gmail.com>
2024-11-12 14:58:45 +0800

    added new project on how to pass data from class

---
+++ how-to-pass-data-from-class/pass-data-from-form-to-class/pass-data-from-form-to-class/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pass_data_from_form_to_class
+{
+    public partial class Form1 : Form
+    {
+        // call class file
+        Class file = new Class();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = Class.static_PassString();
+            label2.Text = file.non_Static_PassString();
+        }
+    }
+}
